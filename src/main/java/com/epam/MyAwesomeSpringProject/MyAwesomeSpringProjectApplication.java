ff8c4be9b1725c2f3a5567f9d25c751357bd6f55
Fedor_Podolinskii <Fedor_Podolinskii@epam.com>
2019-11-13 20:04:50 +0300

    Task7 init

---
+++ src/main/java/com/epam/MyAwesomeSpringProject/MyAwesomeSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.epam.MyAwesomeSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAwesomeSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAwesomeSpringProjectApplication.class, args);
+	}
+
+}
